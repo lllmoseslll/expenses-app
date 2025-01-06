7d4c231fd5ab706eec98e9f185d85a8f26d60a9c
lllmoseslll <mosespeter620@gmail.com>
2025-01-07 01:37:28 +0300

    add global color and editing navigation

---
+++ constants/styles.js
@@ -0,0 +1,17 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#e5ffe5",
+    primary100: "#ccffcc",
+    primary200: "#99ff99",
+    primary400: "#66ff66",
+    primary500: "#33cc33",
+    primary600: "#28a428",
+    primary700: "#1f7a1f",
+    primary800: "#145214",
+    accent500: "#5023b9",
+    error50: "#e5ffe5",
+    error500: "#ff0000",
+    gray500: "#c4ccb3",
+    gray700: "#998f80",
+  },
+};
